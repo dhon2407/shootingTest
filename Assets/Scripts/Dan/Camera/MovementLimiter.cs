ec9acf3b8c807bab59e24be5d3c54d451e57fdf3
Dan Maribojoc <jessiedaniel7@gmail.com>
2021-10-08 10:50:54 +0900

    Lower vertical movement limiter

---
+++ Assets/Scripts/Dan/Camera/MovementLimiter.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Dan.Camera
+{
+    public class MovementLimiter : MonoBehaviour
+    {
+        public float LowerVerticalLimit => transform.position.y;
+    }
+}
--- Assets/Scripts/Dan/Character/Controllers/PlayerController.cs
+++ Assets/Scripts/Dan/Character/Controllers/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using Dan.Camera;
 using Dan.Character.Input;
 using UnityEngine;
 
@@ -14,6 +15,7 @@ namespace Dan.Character.Controllers
 
         private IInputHandler _inputHandler;
         private Vector3 _targetPosition;
+        private MovementLimiter _positionLimiter;
         private bool _active;
         
         public void Activate(bool active)
@@ -31,6 +33,7 @@ namespace Dan.Character.Controllers
         
         private void Awake()
         {
+            _positionLimiter = FindObjectOfType<MovementLimiter>();
             _inputHandler = GetComponent<IInputHandler>();
             _inputHandler.Fire += FireWeapon;
         }
@@ -51,8 +54,11 @@ namespace Dan.Character.Controllers
             while (_active)
             {
                 _targetPosition = transform.position + _inputHandler.MoveVector;
-                transform.position = Vector3.Lerp(transform.position, _targetPosition,
+                _targetPosition = Vector3.Lerp(transform.position, _targetPosition,
                     movementSpeed * Time.deltaTime);
+                
+                _targetPosition.y = Mathf.Max(_targetPosition.y, _positionLimiter.LowerVerticalLimit);
+                transform.position = _targetPosition;
                 yield return null;
             }
         }
